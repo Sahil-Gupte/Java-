578290159cb8c8ef172e9e43e5fb791b59e82eb8
Sahil Gupte <91281882+Sahil-Gupte@users.noreply.github.com>
2024-11-21 20:58:46 +0530

    private to public

---
+++ IntelliStore/src/model/Inventory.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Inventory {
+    private int id;
+    private String name;
+    private int stock;
+    private double price;
+
+    public Inventory(int id, String name, int stock, double price) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
