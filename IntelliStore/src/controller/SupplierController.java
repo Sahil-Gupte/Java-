f40472245f8c9867810e2788104ed444202ee531
Sahil Gupte <91281882+Sahil-Gupte@users.noreply.github.com>
2024-11-21 20:54:40 +0530

    private to public

---
+++ IntelliStore/src/controller/SupplierController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import model.Supplier;
+
+public class SupplierController {
+    // Placeholder for supplier-related methods
+    public void addSupplier(Supplier supplier) {
+        // Implementation for adding a supplier
+    }
+}
