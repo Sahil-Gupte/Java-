425f08c5f2873a595a96be2235fb396379588860
Sahil Gupte <91281882+Sahil-Gupte@users.noreply.github.com>
2024-11-21 21:13:00 +0530

    private to public

--- IntelliStore/src/utils/DatabaseConnection.java
+++ IntelliStore/src/utils/DatabaseConnection.java
@@ -1,1 +1,16 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/intellistore";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
 
